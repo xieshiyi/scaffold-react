53f661cbb0a9f4bef7f51c67ae1e4011924184b9
谢诗意 <xieshiyi@xieshiyi-2.local>
2018-04-09 16:11:25 +0800

    去掉语言配置，默认为英文

--- src/app/app.js
+++ src/app/app.js
@@ -3,13 +3,10 @@ import {render} from 'react-dom'
 import {AppContainer} from 'react-hot-loader'
 import { LocaleProvider } from 'antd'
 import Home from '@pages/Home'
-import zhCN from 'antd/lib/locale-provider/zh_CN';
 const renders = Component => {
     render(
     	<AppContainer>
-    	<LocaleProvider locale={zhCN}>
             <Component/>
-  		</LocaleProvider>
   		</AppContainer>, document.getElementById('app'))
        
 }
